eb5c8d6508f80b9a3f16b5ebb1bf24f4eebdedc9
lucafisc <lucafischer@fastmail.com>
2022-06-27 19:12:14 +0200

    Load nav

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { test } from "../app/test";
-import "./main.css"
+import "./main.css";
+import "./nav.css";
+import { loadNav } from "./nav.js"
+
+const contentDiv = document.querySelector(".content")
+
 test();
+contentDiv.append(loadNav());
---
+++ src/nav.js
@@ -0,0 +1,25 @@
+export const loadNav = () => {
+    const navBar = document.createElement("nav");
+    const logo = document.createElement("img");
+    const navContainer = document.createElement("ul");
+    const homeListItem = document.createElement("li");
+    const menuListItem = document.createElement("li");
+    const aboutListtItem = document.createElement("li");
+
+    navBar.classList.add("nav-bar");
+    logo.classList.add("logo");
+    navContainer.classList.add("nav-container");
+    homeListItem.classList.add("nav-item", "current-page");
+    menuListItem.classList.add("nav-item");
+    aboutListtItem.classList.add("nav-item");
+
+    logo.src = "../images/logo/sushi-logo-01.svg";
+    logo.alt = "Sushi logo";
+    homeListItem.textContent = "Home";
+    menuListItem.textContent = "Menu";
+    aboutListtItem.textContent = "About";
+
+    navContainer.append(homeListItem, menuListItem, aboutListtItem);
+    navBar.append(logo, navContainer);
+    return navBar;
+}
